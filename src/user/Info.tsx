851e20d78367f62e15541a11dd1889699234d1bc
Sebastian Petöcz <sebastian.petoecz@gmail.com>
2022-12-22 14:33:30 +0100

    fix path in index.html

--- src/user/Info.tsx
+++ src/user/Info.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 import InfoTab, {TUserInfo} from "./InfoTab";
-import pdf from '../assets/Bewerbung.pdf';
+//import pdf from '../assets/Bewerbung.pdf';
 
 export default function Info() {
     const [user, setUser] = useState<TUserInfo[]>([]);
@@ -20,7 +20,7 @@ export default function Info() {
                 Hab' ich Dein Interesse geweckt? Dann melde Dich doch einfach bei mir! Oder lade Dir meinen Lebenslauf herunter.
             </span>
             <div className={"flex flex-wrap justify-start mt-8"}>{user.map(exp => <InfoTab {...exp}/>)}</div>
-            <a href={pdf} target="_blank" className={"bg-green-500 w-auto h-20 rounded-br-3xl flex items-center justify-center text-xl font-bold hover:text-white cursor-pointer mt-10"}>Lebenslauf
+            <a href={""} target="_blank" className={"bg-green-500 w-auto h-20 rounded-br-3xl flex items-center justify-center text-xl font-bold hover:text-white cursor-pointer mt-10"}>Lebenslauf
                 herunterladen</a>
         </div>
     );
