dd0e6dfd5123c64c0715b98750d34c6647f25d53
Sebastian Petöcz <sebastian.petoecz@gmail.com>
2023-01-04 14:33:07 +0100

    close navbar when clicking on screen

--- src/navigation/NavigationBar.tsx
+++ src/navigation/NavigationBar.tsx
@@ -12,7 +12,7 @@ import { setCookie } from "react-use-cookie";
 export default function NavigationBar() {
   const { isAuthenticated, getAccessTokenSilently, logout, loginWithPopup } =
     useAuth0();
-  const [openNav, setOpenNav] = useState(false);
+  const [openNav, setOpenNav] = useState<boolean>(false);
 
   useEffect(() => {
     if (isAuthenticated) {
@@ -93,61 +93,69 @@ export default function NavigationBar() {
   );
 
   return (
-    <div
-      className={
-        "flex justify-center items-center w-screen fixed top-7 z-10 px-10"
-      }
-    >
-      <Navbar className="text-imageColor bg-white bg-opacity-70 rounded-2xl shadow-none border-none">
-        <div className="container flex items-center justify-between">
-          <Typography
-            as="a"
-            href="#"
-            className="mr-4 cursor-pointer py-1.5 font-bold text-xl"
-          >
-            <span className={"font-pacifico text-3xl"}>Sebastian Petöcz</span>
-          </Typography>
-          <div className="hidden lg:block">{navList}</div>
-          <IconButton
-            variant="text"
-            className="ml-auto h-6 w-6 text-inherit lg:hidden"
-            ripple={false}
-            onClick={() => setOpenNav(!openNav)}
-          >
-            {openNav ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                className="h-6 w-6"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            )}
-          </IconButton>
-        </div>
-        <MobileNav open={openNav}>{navList}</MobileNav>
-      </Navbar>
+    <div>
+      <div
+        className={
+          "flex justify-center items-center w-screen fixed top-7 z-10 px-10"
+        }
+      >
+        <Navbar className="text-imageColor bg-white bg-opacity-70 rounded-2xl shadow-none border-none">
+          <div className="container flex items-center justify-between">
+            <Typography
+              as="a"
+              href="#"
+              className="mr-4 cursor-pointer py-1.5 font-bold text-xl"
+            >
+              <span className={"font-pacifico text-3xl"}>Sebastian Petöcz</span>
+            </Typography>
+            <div className="hidden lg:block">{navList}</div>
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 text-inherit lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          </div>
+          <MobileNav open={openNav}>{navList}</MobileNav>
+        </Navbar>
+      </div>
+      {openNav && (
+        <div
+          className="fixed inset-0 w-full h-full bg-black opacity-0 z-0"
+          onClick={() => setOpenNav(false)}
+        ></div>
+      )}
     </div>
   );
 }
